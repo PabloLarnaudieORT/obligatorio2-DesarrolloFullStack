fada65e6a418ebce05168621d1c2df1b143b5f20
pablo <pablolarnaudie29@gmail.com>
2026-06-11 19:47:23 -0300

    apuntando a vercel

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,7 @@
-/*
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.open-meteo.com/v1",
-})
-
-export default api;
-*/
-
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000/v1",
+  baseURL: "https://obligatorio1-dfs-seven.vercel.app/",
 });
 
 export default api;
