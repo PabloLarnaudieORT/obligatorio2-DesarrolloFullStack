d734059c4d0859e30c4c11fc97f35fa6f37370a7
pablo <pablolarnaudie29@gmail.com>
2026-06-11 19:51:54 -0300

    faltaba un v1

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://obligatorio1-dfs-seven.vercel.app/",
+  baseURL: "https://obligatorio1-dfs-seven.vercel.app/v1",
 });
 
 export default api;
